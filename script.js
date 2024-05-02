2f6c0bbbeb44b0d70e95bc7df1e727b024d8bb7f
Yusuf Aras <45328601+yusufaras104@users.noreply.github.com>
2024-05-02 07:51:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('https://chat-app-3ftf.onrender.com:3000')
+const socket = io('https://chat-app-3ftf.onrender.com')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
